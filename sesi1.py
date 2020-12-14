ffaf65f1906b027ce09047db5cecee1900d8d20f
ajielinarko <ajielinarko@yahoo.com>
2020-12-14 23:11:53 +0700

    initial Commit add Sesi 1

---
+++ sesi1.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 14 19:56:46 2020
+
+@author: user
+"""
+
+##Training Python
+
+Text = "Kelas Python"
+print(Text)
+
+Text2 = "BMKG"
+print(Text +  Text2)
+
+kardus = ['aqua', 'cocacola', 'sprite', 'fanta']
+print(kardus)
+botol = 'sprite'
+print(botol)
+print(kardus[-4])
